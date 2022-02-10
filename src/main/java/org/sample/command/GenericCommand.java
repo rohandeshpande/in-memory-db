b2478e120bc3f740845b2f17de30ed4dfd9fe4c8
Rohan Deshpande <rohandeshpande2005@gmail.com>
2022-02-10 11:11:01 -0800

    Remove class

--- src/main/java/org/sample/command/GenericCommand.java
+++ src/main/java/org/sample/command/GenericCommand.java
@@ -3,7 +3,6 @@ package org.sample.command;
 public abstract class GenericCommand {
     String variableName;
     Integer value;
-    String output;
 
     public void parse(String input) {
         String[] tokens = input.split(" ");
